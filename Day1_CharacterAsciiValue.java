9c69fc020f3a35637142349ae62d605534c2fc0e
Choudhary-Gopal <109433429+Choudhary-Gopal@users.noreply.github.com>
2023-09-21 17:12:25 +0530

    Add files via upload
    CharacterAsciiValue.java contains code of taking input from command line and printing that input.

---
+++ Day1_CharacterAsciiValue.java
@@ -0,0 +1,32 @@
+class CharacterAsciiValue{
+	public static void main(String args[]){
+              
+	 char ch = 65;
+ 		 System.out.println(ch+"-"+(int)ch++ +'\n'
+                                    + ch+"-"+(int)ch++ +'\n'
+                                    + ch+"-"+(int)ch++ +'\n'
+                                    + ch+"-"+(int)ch++ +'\n'
+                                    + ch+"-"+(int)ch++ +'\n'
+                                    + ch+"-"+(int)ch++ +'\n'
+                                    + ch+"-"+(int)ch++ +'\n'
+                                    + ch+"-"+(int)ch++ +'\n'
+                                    + ch+"-"+(int)ch++ +'\n'
+                                    + ch+"-"+(int)ch++ +'\n'
+                                    + ch+"-"+(int)ch++ +'\n'
+                                    + ch+"-"+(int)ch++ +'\n'
+                                    + ch+"-"+(int)ch++ +'\n'
+                                    + ch+"-"+(int)ch++ +'\n'
+                                    + ch+"-"+(int)ch++ +'\n'
+                                    + ch+"-"+(int)ch++ +'\n'
+                                    + ch+"-"+(int)ch++ +'\n'
+                                    + ch+"-"+(int)ch++ +'\n'
+                                    + ch+"-"+(int)ch++ +'\n'
+                                    + ch+"-"+(int)ch++ +'\n'
+                                    + ch+"-"+(int)ch++ +'\n'
+                                    + ch+"-"+(int)ch++ +'\n'
+                                    + ch+"-"+(int)ch++ +'\n'
+                                    + ch+"-"+(int)ch++ +'\n'
+                                    + ch+"-"+(int)ch++ +'\n'
+                                    + ch+"-"+(int)ch++ +'\n');
+              	}
+}
