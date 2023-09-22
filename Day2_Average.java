e679a7a49f51b9bd597638dc2581610ea5a3ce0e
Choudhary-Gopal <109433429+Choudhary-Gopal@users.noreply.github.com>
2023-09-22 12:36:25 +0530

    Add files via upload
    Average.java contains code of average of 5 number.

---
+++ Day2_Average.java
@@ -0,0 +1,13 @@
+class Average{
+public static void main(String args[]){
+
+   	int   num1 = 15;
+	int   num2 = 25;
+	int   num3 = 35;
+	int   num4 = 45;
+	int   num5 = 55;
+
+		System.out.println("Average of five number is : "+(num1+num2+num3+num4+num5)/5);
+
+	}
+}
