ca0cdf7f673d89c6124494ea42f92d4ab7707c4f
Choudhary-Gopal <109433429+Choudhary-Gopal@users.noreply.github.com>
2023-09-22 15:41:02 +0530

    Add files via upload
    DivisibilityNumbers.java contains code of divisibility check number.

---
+++ Day2_DivisibilityNumbers.java
@@ -0,0 +1,15 @@
+class DivisibilityNumbers{
+public static void main(String args[])
+	{
+ 
+   		int a = 12 ;
+
+	if(a%3==0 && a%7==0)		
+		
+		System.out.println("yes");
+	else
+
+		System.out.println("no");
+
+	}
+}
