35d4149475f8f32ba1a55733dff9d0488848ec3a
Choudhary-Gopal <109433429+Choudhary-Gopal@users.noreply.github.com>
2023-09-23 16:06:19 +0530

    Add files via upload
    PositiveNagative.java contains code of positive and negative numbers.

---
+++ Day3_PositiveNagative.java
@@ -0,0 +1,11 @@
+class PositiveNagative{
+public static void main(String args[]){
+	
+	int x = -4;
+	
+	if(x>=0)
+		System.out.println(x+" :is positive");
+	else
+		System.out.println(x+" :is nagative");
+  }
+}
