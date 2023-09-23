437f67f8a8b8829d1845e465827c58b0667510b3
Choudhary-Gopal <109433429+Choudhary-Gopal@users.noreply.github.com>
2023-09-23 15:57:08 +0530

    Add files via upload
    ConditionalDivisibility.java contains code of  whether number is divisible by 7 or 3.

---
+++ Day3_ConditionalDivisibility.java
@@ -0,0 +1,11 @@
+class ConditionalDivisibility{
+public static void main(String args[]){
+
+	int a = 12;
+
+	if(a%3==0 || a%7==0)
+		System.out.println(a+" :is divisible number");
+	else
+		System.out.println(a+" :is not divisible number");
+ }
+}
