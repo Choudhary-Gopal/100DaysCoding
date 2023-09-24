31a3066e784df1befc30516fe4fb3d82a7fd33ae
Choudhary-Gopal <109433429+Choudhary-Gopal@users.noreply.github.com>
2023-09-24 14:58:07 +0530

    Add files via upload
    Day4_CheckNumber contains code within mentioned range or not.

---
+++ Day4_CheckNumber.java
@@ -0,0 +1,11 @@
+class CheckNumber{
+public static void main(String args[]){
+
+	int a=44;
+	
+	if(a>=21 && a<=575)
+		System.out.println("yes number is valide");
+	else 
+		System.out.println("number is not valid");
+  }
+}
