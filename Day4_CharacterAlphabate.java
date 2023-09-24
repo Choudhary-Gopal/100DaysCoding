bb7f018a3a2d7a46374fddeaa08a7d7587e2abd0
Choudhary-Gopal <109433429+Choudhary-Gopal@users.noreply.github.com>
2023-09-24 14:56:55 +0530

    Add files via upload
    CharacterAlphabate.java contains code of whether given character is alphabate or not

---
+++ Day4_CharacterAlphabate.java
@@ -0,0 +1,12 @@
+class CharacterAlphabate{
+public static void main(String args[]){
+
+	char value = 40 ;
+
+	if(value>=97 && value<=122 || value>=65 && value<=90)
+		System.out.println("given number is valid");
+
+	else
+		System.out.println("given number is not valid");
+  }
+}
