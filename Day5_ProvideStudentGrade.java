a838a32bc9452a8541a4c98d69f2baee3e837967
Choudhary-Gopal <109433429+Choudhary-Gopal@users.noreply.github.com>
2023-09-25 18:39:57 +0530

    Update Day5_ProvideStudentGrade.java

--- Day5_ProvideStudentGrade.java
+++ Day5_ProvideStudentGrade.java
@@ -1,17 +1,39 @@
-  import java.util.Scanner;
+import java.util.Scanner;
 class ProvideStudentGrade{
 public static void main(String args[]){
 
  	Scanner sc = new Scanner(System.in);
 	System.out.print("Enter your percentage :");
-	int per = sc.nextInt();
+	int per1 = sc.nextInt();
 
-if(per>75 && per<=100)
+if(per1>75 && per1<=100)
 	System.out.println("grade is : A ");
-else if(per>40 && per<=75)
+else if(per1>40 && per1<=75)
 	System.out.println("grade is : B");
 else
 	System.out.println("grade is : C");
+
+	System.out.print("Enter your percentage :");
+	int per2 = sc.nextInt();
+
+if(per2>75 && per2<=100)
+	System.out.println("grade is : A ");
+else if(per2>40 && per2<=75)
+	System.out.println("grade is : B");
+else
+	System.out.println("grade is : C");
+
+	System.out.print("Enter your percentage :");
+	int per3 = sc.nextInt();	
+
+if(per3>75 && per3<=100)
+	System.out.println("grade is : A ");
+else if(per3>40 && per3<=75)
+	System.out.println("grade is : B");
+else
+	System.out.println("grade is : C");
+
+
  }
 }
  	 
