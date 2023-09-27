ec87f83977b9395a10d42aacb822339d1bc287ec
Choudhary-Gopal <109433429+Choudhary-Gopal@users.noreply.github.com>
2023-09-27 23:39:37 +0530

    Add files via upload
    Day7_CharacterCheck contains cod of check character .

---
+++ Day7_CharacterCheck.java
@@ -0,0 +1,17 @@
+ import java.util.Scanner;
+class CharacterCheck{
+public static void main(String args[]){
+
+	Scanner sc = new Scanner(System.in);
+	System.out.print("Enter a character: ");
+	char ch = sc.nextLine().charAt(0);
+
+if(ch>='a' && ch<='z' || ch>='A' && ch<='Z')
+	System.out.println("The character is an alphabet");
+else if(ch>='0' && ch<='9')
+	System.out.println("The character is a digit");
+else 
+	System.out.println("The character is special character");
+
+  }
+}
