404a278f87eeac3ddacdae663754c3c3184dd8ad
Choudhary-Gopal <109433429+Choudhary-Gopal@users.noreply.github.com>
2023-09-28 10:09:06 +0530

    Update Day7_CompanyBonus.java

--- Day7_CompanyBonus.java
+++ Day7_CompanyBonus.java
@@ -9,8 +9,8 @@ public static void main(String args[]){
 if(year>=5)
 {
 	System.out.print("Enter employee's salary:");
-	float s = sc.nextInt();
-	System.out.println("your bonus amount is :$ "+s/5);
+	float salary = sc.nextInt();
+	System.out.println("your bonus amount is :$ "+(salary*5)/100;
 }
 else
 	System.out.println("Sorry, you are not eligible for a bonus");
