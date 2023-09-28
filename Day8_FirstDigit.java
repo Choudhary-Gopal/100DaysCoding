62ba7c877dd87c57e63748ec165a27498d1c7d06
Choudhary-Gopal <109433429+Choudhary-Gopal@users.noreply.github.com>
2023-09-28 11:43:47 +0530

    Add files via upload

---
+++ Day8_FirstDigit.java
@@ -0,0 +1,16 @@
+ import java.util.Scanner;
+class FirstDigit{
+public static void main(String args[]){
+
+	Scanner sc = new Scanner(System.in);
+	System.out.print("Enter the number:");
+	int num = sc.nextInt();
+
+while(num>=10){
+
+	num=num/10;
+}
+
+	System.out.println("First digit is:"+num);
+  }
+}
