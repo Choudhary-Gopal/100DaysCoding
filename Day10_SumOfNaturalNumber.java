8f4e770a48594e6b3d87bacc4b8d8cfe18daf41c
Choudhary-Gopal <109433429+Choudhary-Gopal@users.noreply.github.com>
2023-10-01 11:39:32 +0530

    Add files via upload

---
+++ Day10_SumOfNaturalNumber.java
@@ -0,0 +1,12 @@
+class SumOfNaturalNumber{
+public static void main(String args[]){
+int result=0;
+ 
+for(int i=1; i<=5; i++){
+
+result = result + i;
+	System.out.println(i);
+}
+	System.out.print("result="+result);
+  }
+}
