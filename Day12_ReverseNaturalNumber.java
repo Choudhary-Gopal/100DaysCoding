6af345bfc7e8656561a6180cf1479f57c7a36d1b
Choudhary-Gopal <109433429+Choudhary-Gopal@users.noreply.github.com>
2023-10-02 16:28:25 +0530

    Add files via upload

---
+++ Day12_ReverseNaturalNumber.java
@@ -0,0 +1,15 @@
+ import java.util.Scanner;
+class ReverseNaturalNumber{
+public static void main(String args[]){
+
+	Scanner sc = new Scanner(System.in);
+	System.out.print("Enter the number:");
+	int number = sc.nextInt();
+System.out.println("Natural numbers in reverse order from 1 to " + number + ":");
+
+for(int i=1; number>=i; number--)
+
+	System.out.println(number);
+
+   }
+}
