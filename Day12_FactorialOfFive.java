8c4bba33ca36710617e79eaa4318616de03fb3bb
Choudhary-Gopal <109433429+Choudhary-Gopal@users.noreply.github.com>
2023-10-02 16:26:42 +0530

    Add files via upload

---
+++ Day12_FactorialOfFive.java
@@ -0,0 +1,13 @@
+class FactorialOfFive{
+public static void main(String args[]){
+
+int factorial = 1;
+
+for(int i=1; i<=5; i++)
+{
+	factorial = factorial * i;
+}
+	System.out.println("factorial of 5 is:"+factorial);
+
+   }
+}
