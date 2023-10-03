f90d6dddaae1b290696295c2ffa173d3e7c2745b
Choudhary-Gopal <109433429+Choudhary-Gopal@users.noreply.github.com>
2023-10-03 22:48:51 +0530

    Update Day13_FactorialOf_1to5.java

--- Day13_FactorialOf_1to5.java
+++ Day13_FactorialOf_1to5.java
@@ -1,17 +1,14 @@
 class FactorialOf_1to5{
 public static void main(String args[]){
 
-for(int n=1; n<=5; n++)
-{
-    int factorial = 1;
-    for(int i=1; i<=n; i++)
-    {
-	factorial = factorial*i;
-     }
-		
-	System.out.println("Factorial of "+n+" is: "+factorial);
+int factorial = 1;
 
+for(int i=1; i<=5; i++)
+{
+	factorial = factorial * i;
+	System.out.println("Factorial of "+i+" is: "+factorial);
 }
+
 
 
    }
