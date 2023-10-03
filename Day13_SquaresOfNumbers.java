147adce053c836571fe1312a583684f9f0743a84
Choudhary-Gopal <109433429+Choudhary-Gopal@users.noreply.github.com>
2023-10-03 17:16:02 +0530

    Add files via upload

---
+++ Day13_SquaresOfNumbers.java
@@ -0,0 +1,14 @@
+class SquaresOfNumbers{
+public static void main(String args[]){
+
+int square = 1;
+
+for(int i=1; i<=7; i++)
+{
+	square = i*i;
+	System.out.println("Square of "+i+" is: "+square);
+}
+
+
+   }
+}
