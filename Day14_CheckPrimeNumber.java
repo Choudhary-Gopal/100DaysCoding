bc699d584b0fe53e622645542ff32cb3e577db03
Choudhary-Gopal <109433429+Choudhary-Gopal@users.noreply.github.com>
2023-10-05 22:46:43 +0530

    Add files via upload

---
+++ Day14_CheckPrimeNumber.java
@@ -0,0 +1,25 @@
+ import java.util.Scanner;
+class CheckPrimeNumber{
+public static void main(String args[]){
+
+	Scanner sc = new Scanner(System.in);
+	System.out.print("Enter a number: ");
+	int n = sc.nextInt();
+
+boolean b = true;
+
+for(int i=2; i<=n; i++)
+{
+	if(n%i==0)
+		b = false;
+		break;
+	 
+}
+
+	if(b==true)
+		System.out.println(n+" : is prime number");
+	else 
+		System.out.println(n+" : is not prime number");
+
+   }
+}
