82ad1b6b1cca6461967072262aa44b3f1821c10e
Choudhary-Gopal <109433429+Choudhary-Gopal@users.noreply.github.com>
2023-10-06 17:52:54 +0530

    Add files via upload

---
+++ Day15_CheckStrongNumber.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class CheckStrongNumber {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number: ");
+		int number = sc.nextInt();
+		int n1 = number;
+		int sum = 0;
+		int f = 0;
+		
+		while (n1 != 0) {
+			f = n1 % 10;
+			int factors = 1;
+
+			for (int i = 1; i <= f; i++) {
+				factors = factors * i;
+			}
+			sum = sum + factors;
+
+			n1 = n1 / 10;
+		}
+		System.out.println("sum of factors is: " + sum);
+		
+		if (sum == number)
+			System.out.println(number + " : is storng number");
+		else
+			System.out.println(number	 + " : is not strong number");
+	}
+}
