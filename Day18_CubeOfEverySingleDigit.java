33c305223467abc3d9d5032cd6f939ab95bd0e4c
Choudhary-Gopal <109433429+Choudhary-Gopal@users.noreply.github.com>
2023-10-10 00:15:57 +0530

    Add files via upload

---
+++ Day18_CubeOfEverySingleDigit.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class CubeOfEverySingleDigit{
+public static void main(String args[]){
+
+	Scanner sc = new Scanner(System.in);
+	System.out.print("Enter the number: ");
+	int n = sc.nextInt();
+
+int d;
+int revers = 0;
+int cube;
+
+while(n != 0)
+{
+	d = n%10;
+	revers = revers*10 + d;
+	n = n/10;
+}	
+
+while(revers != 0)
+{
+	d = revers%10;
+	cube = d *d *d;
+	System.out.print(cube+" ");
+	revers = revers/10;
+}	
+		
+	
+    }
+}
