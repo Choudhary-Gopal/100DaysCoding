7653ae5b0486d9d7720feebc888b1cbdbc21758d
Choudhary-Gopal <109433429+Choudhary-Gopal@users.noreply.github.com>
2023-10-13 15:50:45 +0530

    Add files via upload

---
+++ Day22_PrintPattern10.java
@@ -0,0 +1,29 @@
+ import java.util.Scanner;
+class PrintPattern10{
+public static void main(String args[]){
+
+	Scanner sc = new Scanner(System.in);
+	System.out.print("Enter the number: ");
+	int n = sc.nextInt();
+        boolean b = false;
+
+for(int i=1; i<=n; i++)
+{
+	for(int j=1; j<=i; j++)
+	{   
+        	if((i+j)%2==0)
+		{
+			System.out.print("1");
+		}
+		else
+		{
+			System.out.print("0");
+		}
+}
+		System.out.println();	 
+}
+
+
+
+   }
+}
