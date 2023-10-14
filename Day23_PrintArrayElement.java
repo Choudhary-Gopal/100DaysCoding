a02084e0f59782b453bd057742ef859a8073bcab
Choudhary-Gopal <109433429+Choudhary-Gopal@users.noreply.github.com>
2023-10-14 17:06:56 +0530

    Add files via upload

---
+++ Day23_PrintArrayElement.java
@@ -0,0 +1,30 @@
+ import java.util.Scanner;
+class PrintArrayElement{
+public static void main(String args[]){
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number: ");
+		int n = sc.nextInt();
+		int[] a = new int[n];
+
+		int j = 0;
+
+		for (int i = 0; i < n; i++) {
+			a[i] = j++;
+		}
+
+		System.out.print("[");
+
+		for (int i = 0; i < n; i++) {
+			System.out.print(a[i]);
+
+			if (i < n - 1) {
+				System.out.print(", ");
+			}
+
+		}
+		System.out.print("]");
+
+
+  }
+}
